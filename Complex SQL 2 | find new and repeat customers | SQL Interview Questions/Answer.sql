bca1ee787dc79162969cc7eb5b4d7de87d5bf695
Bhanu222 <103272212+bhanuprakashNS@users.noreply.github.com>
2023-01-11 23:29:58 +0530

    Update Answer.sql

--- Complex SQL 2 | find new and repeat customers | SQL Interview Questions/Answer.sql
+++ Complex SQL 2 | find new and repeat customers | SQL Interview Questions/Answer.sql
@@ -11,6 +11,8 @@ start_date as (
   	order_date
     ,count(case when co.order_date = sd.ord_date then 1 end) as num_new_customers
     ,count(case when co.order_date != sd.ord_date then 1 end) as num_repeat_customers
+  	,sum(case when co.order_date = sd.ord_date then co.order_amount end) as new_customer_revenue
+    ,sum(case when co.order_date != sd.ord_date then co.order_amount end) as repeat_customer_revenue
   from customer_orders co
   join start_date sd
   	on co.customer_id = sd.customer_id
