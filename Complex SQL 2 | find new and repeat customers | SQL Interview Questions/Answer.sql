753bea82b0bed2201be7cfa7a71dc743514a5f7e
Bhanu222 <103272212+bhanuprakashNS@users.noreply.github.com>
2023-01-11 23:16:43 +0530

    Create Answer.sql

---
+++ Complex SQL 2 | find new and repeat customers | SQL Interview Questions/Answer.sql
@@ -0,0 +1,23 @@
+with
+start_date as (
+  select 
+  	customer_id
+  	,min(order_date) ord_date
+  from customer_orders
+  group by customer_id
+)
+,customer_data as (
+  select
+  	order_date
+    ,count(case when co.order_date = sd.ord_date then 1 end) as num_new_customers
+    ,count(case when co.order_date != sd.ord_date then 1 end) as num_repeat_customers
+  from customer_orders co
+  join start_date sd
+  	on co.customer_id = sd.customer_id
+  group by order_date
+)
+select * from customer_data
+
+
+  
+   
