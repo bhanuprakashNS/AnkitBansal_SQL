10fa22344c166e46f96dc509d68291b116cb18e8
Bhanu222 <103272212+bhanuprakashNS@users.noreply.github.com>
2023-01-28 23:55:55 +0530

    Update table_info.sql

--- Complex SQL 5 | Pareto Principle (80 to 20 Rule) Implementation in SQL | PBC Interview Question/table_info.sql
+++ Complex SQL 5 | Pareto Principle (80 to 20 Rule) Implementation in SQL | PBC Interview Question/table_info.sql
@@ -1,1 +1,1 @@
-(Data_Link)[https://docs.google.com/spreadsheets/d/1TPlQzPs9kOZkNVJO0-9Jwx01rp4VUAuM/edit?usp=share_link&ouid=105415796962620516055&rtpof=true&sd=true]
+[Data_Link](https://docs.google.com/spreadsheets/d/1TPlQzPs9kOZkNVJO0-9Jwx01rp4VUAuM/edit?usp=share_link&ouid=105415796962620516055&rtpof=true&sd=true)
